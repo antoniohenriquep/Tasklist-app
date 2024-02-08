a2b50192e86205777982348123225a903877ad1f
antoniohenriquep <antoniohenriquep@outlook.com>
2024-02-08 20:48:40 -0300

    feat: Pagina de registro

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import api from './src/services/api';
 import Login from './src/pages/Login';
+import Signup from './src/pages/Signup';
 
 export default function App()
 {
   return(
-    <Login/>
+    //<Login/>
+    <Signup/>
   )
 }
